a4684a271fd3b705e24dc90b1567d47fd39d003b
Devi Sri Charan <vdevisricharan@gmail.com>
2026-06-20 07:50:43 +0000

    Created Component Folders

---
+++ src/App.js
@@ -0,0 +1,39 @@
+// src/App.js
+import {Route, Switch, Redirect} from 'react-router-dom'
+import Login from './components/Login'
+import Home from './components/Home'
+import Cart from './components/Cart'
+import RestaurantDetails from './components/RestaurantDetails'
+import NotFound from './components/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
+import './App.css'
+
+const sortByOptions = [
+  {
+    id: 0,
+    displayText: 'Highest',
+    value: 'Highest',
+  },
+  {
+    id: 2,
+    displayText: 'Lowest',
+    value: 'Lowest',
+  },
+]
+
+const App = () => (
+  <Switch>
+    <Route exact path="/login" component={Login} />
+    <ProtectedRoute exact path="/" component={Home} />
+    <ProtectedRoute
+      exact
+      path="/restaurant/:id"
+      component={RestaurantDetails}
+    />
+    <ProtectedRoute exact path="/cart" component={Cart} />
+    <Route path="/not-found" component={NotFound} />
+    <Redirect to="/not-found" />
+  </Switch>
+)
+
+export default App
